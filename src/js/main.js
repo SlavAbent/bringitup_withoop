addbddf951c263e83b85f115266caa6f72f676a1
SlavAbent <slavabent2015@gmail.com>
2021-04-15 11:46:06 +0500

    add slider oop

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+import Slider from './modules/slider.js'
+=======
+import Slider from './modules/slider'
+>>>>>>> aea2460acff1d91acebacda577f2046e95f17061
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const slider = new Slider('.page', '.next');
+    slider.render()
+})
