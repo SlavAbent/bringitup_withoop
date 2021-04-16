88ab9ff1ad0b9b5b7231316c568a49385743093a
SlavAbent <slavabent2015@gmail.com>
2021-04-16 10:45:24 +0500

    add videoplayer on mainpage

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider.js'
+import VideoPlayer from './modules/playVideo'
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render()
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 })
